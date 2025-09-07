71c326d2bf5d7e4a7d3a2893014eab2cc246af1e
irish199718 <gudesirish2@gmail.com>
2025-09-07 19:11:31 +0800

    added user input handling for menu choice

--- hello.js
+++ hello.js
@@ -12,3 +12,6 @@ console.log("1. Say Hello");
 console.log("2. Factorial");
 console.log("3. Exit Application");
 console.log("---------------------------");
+
+rl.question("Enter your choice (1-3): ", choice =>{
+    console.log(choice);
