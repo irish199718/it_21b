5fc0004346cca6214b45a01289915106ad0538af
irish199718 <gudesirish2@gmail.com>
2025-09-07 19:09:54 +0800

    added basic CLI menu for fatorial application

--- hello.js
+++ hello.js
@@ -1,13 +1,14 @@
-console.log ("hello world")
+//Console Input
+const readline=require("readline");
 
-num1 = 2;
-num2 = 3;
+const rl = readline.createInterface({
+    input:process.stdin,
+    output: process.stdout
+})
 
-sum = num1 + num2;
-diff = num1 - num2;
-prod = num1 * num2;
-quo = num1 / num2;
-console.log("The sum of" + num1 + " & " + num2 + " is " + sum);
-console.log("The difference of" + num1 + " & " + num2 + " is " +diff);
-console.log("The product of" + num1 + " & " + num2 + " is " + prod);
-console.log("The quotient of" + num1 + " & " + num2 + " is " + quo);
+console.log("---------------------------");
+console.log("Factorial Application");
+console.log("1. Say Hello");
+console.log("2. Factorial");
+console.log("3. Exit Application");
+console.log("---------------------------");
