5e964bc289af8375347c5525a4a07c863ecea53d
irish199718 <gudesirish2@gmail.com>
2025-09-09 21:36:02 +0800

    added factorial computation with input validation and menu return

--- hello.js
+++ hello.js
@@ -34,5 +34,22 @@ function sayHello(){
 
 function computeFactorial(){
     console.log("Factorial");
-    
+
+     rl.question("Please enter a number for factorial: ", numStr =>{
+            let num = parseInt(numStr);
+
+            if (isNaN(num) || num < 0){
+                console.log("Please enter a non-negative integer");
+            } else {
+                let fact = 1;
+
+                for (let i=1; i <= num; i++){
+                    fact *=i;
+                }
+
+                console.log("The factorial of " + num + " is " + fact);
+            }
+            backToMenu();
+        })
 }
+
