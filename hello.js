fa0362935ecc10b0c19e0e7052405ad84272f1c3
irish199718 <gudesirish2@gmail.com>
2025-09-09 21:31:28 +0800

    added mainMenu function with user choice handling

--- hello.js
+++ hello.js
@@ -5,3 +5,25 @@ const rl = readline.createInterface({
     input:process.stdin,
     output: process.stdout
 })
+function mainMenu(){
+    console.log("---------------------------");
+    console.log("Factorial Application");
+    console.log("1. Say Hello");
+    console.log("2. Factorial");
+    console.log("3. Exit Application");
+    console.log("---------------------------");
+
+    rl.question("Enter your choice (1-3): ", choice =>{
+    
+        if (choice === "1"){
+           sayHello();
+        } else if (choice === "2"){
+           computeFactorial();
+        } else if (choice === "3"){
+            exitProgram();
+        } else {
+            console.log("Invalid Choice. Please Try Again");
+        }
+
+    })
+}
